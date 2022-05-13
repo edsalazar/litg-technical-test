9cb260c0362bcfd6ee3c91843de3d8fee2211ac9
edsalazar <edsalazar88@gmail.com>
2022-05-13 18:01:25 -0500

    Working on guns mechanics

--- Assets/Scripts/GameNames.cs
+++ Assets/Scripts/GameNames.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 public static class GameNames
 {
 
+    // Tags
+
+    public const string GunModel = "GunModel";
+
     // Animator paramters
     public const string isHouseDance = "isHouseDance";
     public const string isMacarenaDance = "isMacarenaDance";
     public const string isWaveHipHopDance = "isWaveHipHopDance";
+
+    // Gun Models
+    public const string Gun1Model = "Gun1Model";
+    public const string Gun2Model = "Gun2Model";
+
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject[] guns;
+    public GameObject[] gunModels;
+
+    public GameObject currentGun;
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.CompareTag(GameNames.GunModel))
+        {
+            ActivateSelectedGun(hit.gameObject);
+        }
+    }
+
+    void ActivateSelectedGun(GameObject gunModel)
+    {
+        DropCurrentGun(currentGun);
+
+        if (gunModel.name.Equals(GameNames.Gun1Model))
+        {
+            currentGun = guns[0];
+
+            guns[0].SetActive(true);
+            guns[1].SetActive(false);
+        }
+
+        else if (gunModel.name.Equals(GameNames.Gun2Model))
+        {
+            currentGun = guns[1];
+
+            guns[1].SetActive(true);
+            guns[0].SetActive(false);
+
+        }
+
+        gunModel.SetActive(false);
+    }
+
+    void DropCurrentGun(GameObject currentGun)
+    {
+        if(currentGun != null)
+        {
+            if (currentGun.Equals(guns[0]))
+            {
+                gunModels[0].transform.localPosition = new Vector3(transform.position.x, 0.3f, transform.position.z) - Vector3.forward; ;
+                gunModels[0].SetActive(true);
+            }
+
+            else if (currentGun.Equals(guns[1]))
+            {
+                gunModels[0].transform.localPosition = new Vector3(transform.position.x, 0.3f, transform.position.z) - Vector3.forward;
+                gunModels[1].SetActive(true);
+            }
+        }
+    }
+}
